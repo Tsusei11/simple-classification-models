3f40ef41657d67dbcb5b8683bdbb14755abde816
Tsusei11 <147321373+Tsusei11@users.noreply.github.com>
2025-04-07 18:09:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,29 @@ train = '../data/iris_training.txt'
 test = '../data/iris_test.txt'
 perceptron_decision = 'Iris-setosa'
 articles = '../data/articles/'
+
+letters_alt = {
+  'ę': 'e',
+  'ą': 'a',
+  'ł': 'l',
+  'ó': 'o',
+  'ń': 'n',
+  'ż': 'z',
+  'ź': 'z',
+  'ä': 'a',
+  'ö': 'o',
+  'ü': 'u',
+  'à': 'a',
+  'â': 'a',
+  'é': 'e',
+  'è': 'e',
+  'ê': 'e',
+  'ë': 'e',
+  'î': 'i',
+  'ï': 'i',
+  'ô': 'o',
+  'ù': 'u',
+  'û': 'u',
+  'ÿ': 'y',
+  'ç': 'c',
+}
